a614799dd7a2367fd377e52111e3bae007fa2bd1
IPIvliev <cccp1945@mail.ru>
2013-08-14 22:10:34 +0300

    Start work with sity for Infocom

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infocom::Application.config.secret_token = 'b46e70b986c7cd4c249fc5483578e5d24461a02aabb7afe38965d4c256621159942f1a8f53fd6bb9f01fc30041882643083057f2d6ac45aff79f0687e58305fa'
