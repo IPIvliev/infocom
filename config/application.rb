6c981c7ce84948401840a070dea8bad6c44c5639
IPIvliev <cccp1945@mail.ru>
2013-08-17 17:07:48 +0300

    Modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 
+
 require 'rails/all'
 
 if defined?(Bundler)
@@ -11,6 +12,7 @@ if defined?(Bundler)
 end
 
 module Infocom
+  class Application < Rails::Application
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
