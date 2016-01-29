746e92fd0336cd715520ee6c360f00256eefcc93
IPIvliev <cccp1945@mail.ru>
2016-01-29 11:09:35 +0300

    Change kp and phone number

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -7,7 +7,7 @@ class MarketingMailer < ActionMailer::Base
 
   def kp_email(mail)
 
-  	attachments["kp_infocom_nn.docx"] = File.read('public/kp_infocom_nn.docx', :mode => 'rb')
+  	attachments["kp_infocom_nn.pdf"] = File.read('public/kp_infocom_nn.pdf', :mode => 'rb')
 
     mail(to: mail, subject: 'Коммерческое предложение по ИТ-аутсорсингу от Инфоком-НН')
   end
